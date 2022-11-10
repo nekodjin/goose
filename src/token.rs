25d2aae3a897479953ccae6fddf436e09b3d0b3d
Avery R <contact@nekodjin.org>
2022-11-10 18:33:51 -0500

    dfjhaskhfdaksj

--- src/token.rs
+++ src/token.rs
@@ -1,19 +1,7 @@
 use crate::intern::*;
 
-use logos::{Lexer, Logos};
-
-#[derive(Logos, Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Token {
-    #[error]
-    #[regex(r"[ \t\r\n]+", logos::skip)]
-    Error,
-
-    #[regex(r"\$[_a-zA-Z0-9]+", cb_name)]
     Name(IString),
     // TODO: URL regex
     Url(IString),
-}
-
-fn cb_name(l: &mut Lexer<Token>) -> IString {
-    l.slice()[1..].id()
 }
